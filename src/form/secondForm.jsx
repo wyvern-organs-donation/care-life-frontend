3162f5acff9c0d948abbeca82fe083c4f0af38e3
devartes <anacavalcante731@gmail.com>
2022-11-03 10:42:42 -0300

    adicionando primeira estrutura do 2° formulário

---
+++ src/form/secondForm.jsx
@@ -0,0 +1,46 @@
+import './secondForm.css'
+
+function SecondForm() {
+    return (
+        <div className="FormContent">
+            <h3>Bem-vindo</h3>
+            <h4>Doe orgãos, doe vida</h4>
+            <form>
+                <label>termo de autorização de doação de órgãos e tecidos</label>
+                <div className="NumbersFormPosition">
+                    <div className="NumbersForm">
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">7<span className="SmallNumber">/15</span></span>
+                        </div>
+                        <div className="NumbersFormCircleBlue">
+                            <span className="BigNumberBlue">8<span className="SmallNumberBlue">/15</span></span>
+                        </div>
+                        <div className="NumbersFormCircleBlue">
+                            <span className="BigNumberBlue">9<span className="SmallNumberBlue">/15</span></span>
+                        </div>                   
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">10<span className="SmallNumber">/15</span></span>
+                        </div>
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">11<span className="SmallNumber">/15</span></span>
+                        </div>
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">12<span className="SmallNumber">/15</span></span>
+                        </div>                  
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">13<span className="SmallNumber">/15</span></span>
+                        </div>                  
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">14<span className="SmallNumber">/15</span></span>
+                        </div>                  
+                        <div className="NumbersFormCircle">
+                            <span className="BigNumber">15<span className="SmallNumber">/15</span></span>
+                        </div>                  
+                    </div>
+                </div>             
+            </form>
+        </div>
+    )
+}
+
+export default SecondForm
