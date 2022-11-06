9855bdeef832a2dc90bf3486105b6f3abc3a2b82
devartes <anacavalcante731@gmail.com>
2022-11-06 17:20:04 -0300

    adicionando otp input

---
+++ src/form/OtpInput.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const OTPBox = () => {
+    const [otp, setOtp] = useState(new Array(11).fill(""));
+
+    const handleChange = (element, index) => {
+        if (isNaN(element.value)) return false;
+        setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+        if (element.nextSibling) {
+            element.nextSibling.focus();
+        }
+    };
+
+    return (
+        <>
+            <div>                   
+                    {otp.map((data, index) => {
+                        return (
+                            <input
+                                className="otp-field"
+                                type="text"
+                                name="otp"
+                                maxLength="1"
+                                key={index}
+                                value={data}
+                                onChange={e => handleChange(e.target, index)}
+                                onFocus={e => e.target.select()}
+                            />
+                        );
+                    })}              
+            </div>
+        </>
+    );
+};
+
+export default OTPBox;
--- src/form/secondForm.jsx
+++ src/form/secondForm.jsx
@@ -1,3 +1,4 @@
+import OTPBox from './OtpInput'
 import './secondForm.css'
 
 function SecondForm() {
@@ -6,38 +7,100 @@ function SecondForm() {
             <h3>Bem-vindo</h3>
             <h4>Doe orgãos, doe vida</h4>
             <form>
-                <label>termo de autorização de doação de órgãos e tecidos</label>
+                <label className="SubTitle">termo de autorização de doação de órgãos e tecidos</label>
                 <div className="NumbersFormPosition">
                     <div className="NumbersForm">
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">7<span className="SmallNumber">/15</span></span>
+                        <div className="NumbersFormContent">
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">7<span className="SmallNumber">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircleBlue">
+                                <span className="BigNumberBlue">8<span className="SmallNumberBlue">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircleBlue">
+                                <span className="BigNumberBlue">9<span className="SmallNumberBlue">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">10<span className="SmallNumber">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">11<span className="SmallNumber">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">12<span className="SmallNumber">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">13<span className="SmallNumber">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">14<span className="SmallNumber">/15</span></span>
+                            </div>
+                            <div className="NumbersFormCircle">
+                                <span className="BigNumber">15<span className="SmallNumber">/15</span></span>
+                            </div>
                         </div>
-                        <div className="NumbersFormCircleBlue">
-                            <span className="BigNumberBlue">8<span className="SmallNumberBlue">/15</span></span>
+                        <div className="NumbersFormInformation">
+                            <span>Respónsavel pela doação</span>
+                            <span>RG</span>
+                            <span>CPF</span>
+                            <span>Sexo</span>
+                            <span>Endereço</span>
+                            <span>Autorização Judicial confirmação</span>
+                            <span>Telefone</span>
+                            <span>Email</span>
+                            <span>Autorização de consentimento</span>
                         </div>
-                        <div className="NumbersFormCircleBlue">
-                            <span className="BigNumberBlue">9<span className="SmallNumberBlue">/15</span></span>
-                        </div>                   
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">10<span className="SmallNumber">/15</span></span>
+                        <div className="NumbersFormData">
+                            <div className="SelectStyle">
+                                <select>
+                                    <option value="0">Escolha uma opção</option>
+                                    <option value="1">Mãe</option>
+                                    <option value="2">Pai</option>
+                                </select>
+                            </div>
+                            <div className="userInput">
+                               <OTPBox/>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opção</option>
+                                </select>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opçãoffkfkfffkf</option>
+                                </select>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opção</option>
+                                </select>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opçãoffkfkfffkf</option>
+                                </select>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opção</option>
+                                </select>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opçãoffkfkfffkf</option>
+                                </select>
+                            </div>
+                            <div>
+                                <select>
+                                    <option value="">Escolha uma opçãoffkfkfffkf</option>
+                                </select>
+                            </div>
                         </div>
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">11<span className="SmallNumber">/15</span></span>
-                        </div>
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">12<span className="SmallNumber">/15</span></span>
-                        </div>                  
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">13<span className="SmallNumber">/15</span></span>
-                        </div>                  
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">14<span className="SmallNumber">/15</span></span>
-                        </div>                  
-                        <div className="NumbersFormCircle">
-                            <span className="BigNumber">15<span className="SmallNumber">/15</span></span>
-                        </div>                  
+
+
                     </div>
-                </div>             
+                </div>
             </form>
         </div>
     )
