6150de7d40dd20d782105a0cb0ab03218f765cb8
GilvaniaMoreira <moreiragil84@gmail.com>
2022-11-13 01:04:16 -0300

    Ajustando link navbar admin

--- src/componets/admin/admin_aplicacao/navbar/navbar.jsx
+++ src/componets/admin/admin_aplicacao/navbar/navbar.jsx
@@ -8,11 +8,8 @@ function Navbar() {
       <nav className="navbar">
         <Link to="/"><img src="/src/assets/admin/gg_menu-cake.svg" alt="menu" /></Link>
         <Link to="/admim"><img src="/src/assets/admin/ci_dashboard.svg" alt="dashboard" /></Link>
-        <a href="#"><img src="/src/assets/admin/entypo_bar-graph.svg" alt="grafico" /></a>
-        <a href="#"><img src="/src/assets/admin/ci_settings-filled.svg" alt="configurações" /></a>
-        <a href="#"><img src="/src/assets/admin/iconoir_profile-circled.svg" alt="usuarios" /></a>
-        <a href="#"><img src="/src/assets/admin/material-symbols_auto-schedule-sharp.svg" alt="agenda" /></a>
-        <a href="#"><img src="/src/assets/admin/Vector.svg" alt="sair" /></a>
+        <Link to="/"><img src="/src/assets/admin/iconoir_profile-circled.svg" alt="usuarios" /></Link>
+        <Link to="/"><img src="/src/assets/admin/Vector.svg" alt="sair" /></Link>
       </nav>
     </div>
   )
