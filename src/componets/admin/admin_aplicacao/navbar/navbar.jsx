0fce47ceaf5b906abcb5d94c2b3849749ccf3eb7
GilvaniaMoreira <moreiragil84@gmail.com>
2022-11-12 22:42:50 -0300

    Add link no navbar admin

--- src/componets/admin/admin_aplicacao/navbar/navbar.jsx
+++ src/componets/admin/admin_aplicacao/navbar/navbar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './navbar.css'
 
 function Navbar() {
   return (
     <div className='rowC'>
-      <div className="navbar">
-        <a href="#"><img src="/src/assets/admin/gg_menu-cake.svg" alt="menu" /></a>
-        <a href="#"><img src="/src/assets/admin/ci_dashboard.svg" alt="dashboard" /></a>
+      <nav className="navbar">
+        <Link to="/"><img src="/src/assets/admin/gg_menu-cake.svg" alt="menu" /></Link>
+        <Link to="/admim"><img src="/src/assets/admin/ci_dashboard.svg" alt="dashboard" /></Link>
         <a href="#"><img src="/src/assets/admin/entypo_bar-graph.svg" alt="grafico" /></a>
         <a href="#"><img src="/src/assets/admin/ci_settings-filled.svg" alt="configurações" /></a>
-        <a href="#"><img src="/src/assets/admin/iconoir_profile-circled.svg" alt="grafico" /></a>
+        <a href="#"><img src="/src/assets/admin/iconoir_profile-circled.svg" alt="usuarios" /></a>
         <a href="#"><img src="/src/assets/admin/material-symbols_auto-schedule-sharp.svg" alt="agenda" /></a>
         <a href="#"><img src="/src/assets/admin/Vector.svg" alt="sair" /></a>
-      </div>
+      </nav>
     </div>
   )
 }
--- src/componets/admin/instituicao/navbar/navbar.jsx
+++ src/componets/admin/instituicao/navbar/navbar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import './navbar.css'
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 function Navbar() {
   return (
     <div className='rowC'>
-      <div className="navbari">
-        <a href="#"><img src="/src/assets/admin/gg_menu-cake.svg" alt="menu" /></a>
-        <a href="#"><img src="/src/assets/admin/ci_dashboard.svg" alt="dashboard" /></a>
+      <nav className="navbari">
+        <Link to="/"><img src="/src/assets/admin/gg_menu-cake.svg" alt="menu" /></Link>
+        <Link to="/admim"><img src="/src/assets/admin/ci_dashboard.svg" alt="dashboard" /></Link>
         <a href="#"><img src="/src/assets/admin/entypo_bar-graph.svg" alt="grafico" /></a>
         <a href="#"><img src="/src/assets/admin/ci_settings-filled.svg" alt="configurações" /></a>
         <a href="#"><img src="/src/assets/admin/iconoir_profile-circled.svg" alt="grafico" /></a>
         <a href="#"><img src="/src/assets/admin/material-symbols_auto-schedule-sharp.svg" alt="agenda" /></a>
         <a href="#"><img src="/src/assets/admin/Vector.svg" alt="sair" /></a>
-      </div>
+      </nav>
     </div>
   )
 }
