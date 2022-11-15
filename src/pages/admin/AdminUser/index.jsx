ee241eb60f220bae5d83f6c365b08898e0fe7121
GilvaniaMoreira <moreiragil84@gmail.com>
2022-11-15 01:16:07 -0300

    Iniciando a integração com o back

--- src/pages/admin/AdminUser/index.jsx
+++ src/pages/admin/AdminUser/index.jsx
@@ -1,11 +1,25 @@
+import React, { useEffect, useState } from "react";
 import Header from '../components/Header';
 import Navbar from '../components/BlueNavbar/navbar';
 import Buscar from './components/buscar/buscar';
 import Button from './components/buttons/buttons';
+import GetUsers from './components/users';
 import './style.css'
 import { Container } from 'react-bootstrap';
+import api from "../../../services/api";
 
 function AdminUser() {
+    const [users, setUser] = useState([]);
+
+    useEffect(() => {
+        api
+        .get("/user")
+        .then((response) => setUser(response.data))
+        .catch((err) => {
+            console.error("ops! ocorreu um erro" + err);
+        });
+    }, []);
+    
     return (
         <div className='rowC'>
             <Navbar />
@@ -13,7 +27,9 @@ function AdminUser() {
                 <Header />
                 <Buscar />
                 <Button />
+                <GetUsers users={users} />
             </Container>
+            
         </div>
     )
 }
