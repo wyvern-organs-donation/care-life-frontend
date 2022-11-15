7b4034ba306c94556da716bbeb96624df012f5a4
GilvaniaMoreira <moreiragil84@gmail.com>
2022-11-15 14:19:03 -0300

    Adicionando login integrado com API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Admin from './pages/admin/Admin';
 import AdminUser from './pages/admin/AdminUser';
 import AdminApplication from './pages/admin/AdminApplication';
@@ -6,19 +7,66 @@ import Register from "./pages/register/user-register";
 import DonorRegister from "./pages/register/donor-register";
 import ConfirmRegister from "./pages/register/confirm-register";
 import "./pages/login/index";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SecondForm from './form/SecondForm';
 
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(localStorage.getItem("user"));
+  
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      setUser(loggedInUser);
+    }
+  }, []);
+
+  const ProtectedRoute = ({
+    isAllowed,
+    redirectPath = '/',
+    children,
+  }) => {
+    if (!isAllowed) {
+      return <Navigate to={redirectPath} replace />;
+    }
+  
+    return children;
+  };
+
   return (
       <BrowserRouter>
         <Routes>
           <Route path='/'element={<SecondForm />} />
-          <Route path='/admin'element={<Admin />} />
-          <Route path='/admin-user'element={<AdminUser/>} />
-          <Route path='/admin-application'element={<AdminApplication/>} />
+          <Route path='/admin'element={
+            <ProtectedRoute
+              redirectPath="/"
+              isAllowed={
+                !!user
+              }
+            >
+              <Admin />
+            </ProtectedRoute>
+          } />
+          <Route path='/admin-user'element={
+            <ProtectedRoute
+              redirectPath="/"
+              isAllowed={!!user}
+            >
+              {console.log(!!user)}
+              <AdminUser />
+            </ProtectedRoute>
+          } />
+          <Route path='/admin-application'element={
+            <ProtectedRoute
+              redirectPath="/"
+              isAllowed={
+                !!user
+              }
+            >
+              <AdminApplication />
+            </ProtectedRoute>
+          } />
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/donor-register" element={<DonorRegister />}></Route>
--- src/pages/admin/AdminUser/components/users/index.jsx
+++ src/pages/admin/AdminUser/components/users/index.jsx
@@ -7,9 +7,9 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TableContainer from '@mui/material/TableContainer';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
   
 function GetUsers(props) {
-    console.log(props.users)
     return (
         <Container maxWidth="lg">
             <TableContainer>
@@ -26,11 +26,13 @@ function GetUsers(props) {
                 </TableHead>
                 <TableBody>
                     {props.users.map((user) => (
-                    <TableRow key={user.ID}>
-                        <ButtonGroup color="primary" aria-label="outlined primary button group">
-                            <Button onClick={() => UpdateUser(user.id)}>Edit</Button>
-                            <Button onClick={() => UserDelete(user.id)}>Del</Button>
-                        </ButtonGroup>
+                    <TableRow key={user.id}>
+                        <TableCell align="center">
+                            <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                <Button onClick={() => props.userUpdate(user.id)}>Edit</Button>
+                                <Button onClick={() => props.userDelete(user.id)}>Del</Button>
+                            </ButtonGroup>
+                        </TableCell>
                         <TableCell align="right">{user.phone_number}</TableCell>
                         <TableCell align="left">{user.type}</TableCell>
                         <TableCell align="left">{user.birth_date}</TableCell>
