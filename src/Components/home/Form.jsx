86475a2af4c10ec24d6863b316c101d0e1e3006d
Gabriel <gabriel2000.silva.sa@gmail.com>
2022-11-17 21:44:13 -0300

    ajustes

--- src/Components/home/Form.jsx
+++ src/Components/home/Form.jsx
@@ -4,7 +4,7 @@ export function Form(){
     return(
         <div id="form">
             <form>
-                <h1>Tire suas dúvidas</h1>
+                <h2>Tire suas dúvidas</h2>
                 <input type="name" id="name" placeholder="Nome Completo"></input>
                 <input type="email" id="email" placeholder="E-mail"></input>
                 <input type="text" id="doubt" placeholder="Digite sua dúvida aqui"></input>
