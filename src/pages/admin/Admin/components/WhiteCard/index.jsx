1dcb0d0274075b8cecb6078ca9c6f29be0845276
GilvaniaMoreira <moreiragil84@gmail.com>
2022-11-24 20:42:50 -0300

    adicionando links

--- src/pages/admin/Admin/components/WhiteCard/index.jsx
+++ src/pages/admin/Admin/components/WhiteCard/index.jsx
@@ -11,7 +11,7 @@ function WhiteCard({title, imgSrc, count}) {
         <span className="count">{count}</span>
       </div>
       <h3>{title}</h3>
-      <a className="card-link" href='#'>Saiba Mais<img src="/src/assets/admin/arrow-right.svg" /></a>
+      <a className="card-link" href='/admin-user'>Saiba Mais<img src="/src/assets/admin/arrow-right.svg" /></a>
     </div>
   );
 }
